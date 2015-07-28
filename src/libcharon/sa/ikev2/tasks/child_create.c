rlupton20-msft/strongswan

commit 32df0d81fb468861c67de7fda6d4fd21d90241b5

child-create: Destroy nonceg in migrate()
Since another nonce gets allocated later (if any was allocated already)
this would have resulted in a leaked nonce context ID when used in charon-tkm.

src/libcharon/sa/ikev2/tasks/child_create.c
@@ -221,6 +221,7 @@ static status_t get_nonce(message_t *message, chunk_t *nonce)
  */
 static bool generate_nonce(private_child_create_t *this)
 {
+	this->nonceg = this->keymat->keymat.create_nonce_gen(&this->keymat->keymat);
 	if (!this->nonceg)
 	{
 		DBG1(DBG_IKE, "no nonce generator found to create nonce");
@@ -1580,6 +1581,7 @@ METHOD(task_t, migrate, void,
 	}
 	DESTROY_IF(this->child_sa);
 	DESTROY_IF(this->proposal);
+	DESTROY_IF(this->nonceg);
 	DESTROY_IF(this->dh);
 	this->dh_failed = FALSE;
 	if (this->proposals)
@@ -1671,7 +1673,6 @@ child_create_t *child_create_create(ike_sa_t *ike_sa,
 		.rekey = rekey,
 		.retry = FALSE,
 	);
-	this->nonceg = this->keymat->keymat.create_nonce_gen(&this->keymat->keymat);
 
 	if (config)
 	{

commit 78bab0b68254accb48f08c5110a904a0dedabc60

child-create: Fix crash when retrying CHILD_SA rekeying due to a DH group mismatch
If the responder declines our KE payload during a CHILD_SA rekeying migrate()
is called to reuse the child-create task.  But the child-rekey task then
calls the same method again.

Fixes: 32df0d81fb46 ("child-create: Destroy nonceg in migrate()")

src/libcharon/sa/ikev2/tasks/child_create.c
@@ -1596,6 +1596,7 @@ METHOD(task_t, migrate, void,
 	this->tsi = NULL;
 	this->tsr = NULL;
 	this->dh = NULL;
+	this->nonceg = NULL;
 	this->child_sa = NULL;
 	this->mode = MODE_TUNNEL;
 	this->ipcomp = IPCOMP_NONE;